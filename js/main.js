abff17d03079924a3f59404ca15b67537d717b0b
Daniel_M_Williams <equipoise@gmail.com>
2020-01-14 08:38:06 -0500

    [feat] Tool Sidebar now  has subsections

--- js/main.js
+++ js/main.js
@@ -1,28 +1,21 @@
 'use strict'
 
-let activeToolDiv = null
-let activeTexId = 0
-let isPlacing = false
-
 // not sure where this comes from...
 /* global history */
 
-const click = (canvas, map, event) => {
-  const { i, j } = getTileIndex(canvas, event.offsetX, event.offsetY)
-
-  if (activeTexId < 0) {
-    return
-  }
+const click = (interfaceLayer, map, event) => {
+  const { i, j } = getTileIndex(interfaceLayer, event.offsetX, event.offsetY)
 
   if ((i >= 0 && i < map.dimension) && (j >= 0 && j < map.dimension)) {
     if (event.which === 3) {
       map.tiles[i][j].textureId = 0
+    } else if (interfaceLayer.activeToolId >= 0) {
+      map.tiles[i][j].textureId = interfaceLayer.activeToolId
     } else {
-      map.tiles[i][j].textureId = activeTexId
+      return
     }
 
-    isPlacing = true
-    drawMap(canvas, map)
+    interfaceLayer.isPlacing = true
   }
 
   const hash = map.save()
@@ -30,9 +23,9 @@ const click = (canvas, map, event) => {
   history.replaceState(undefined, undefined, `#${hash}`)
 }
 
-const unclick = e => {
-  if (isPlacing) {
-    isPlacing = false
+const unclick = (_, interfaceLayer) => {
+  if (interfaceLayer.isPlacing) {
+    interfaceLayer.isPlacing = false
   }
 }
 
@@ -91,15 +84,23 @@ const drawMap = (canvas, map) => {
   }
 }
 
-const drawCursor = (canvas, i, j) => {
-  const context = canvas.getContext('2d')
-  context.resetTransform()
+const drawActiveTool = (context, texture) => {
+  if (typeof texture === 'undefined' || texture === null) {
+    // this is a common case, and happens on startup
+    // don't worry... but don't try to draw anything :)
+    return
+  }
 
-  const topBorder = 130
+  const h = texture.source.h
+  const w = texture.source.w
+  context.clearRect(0, 0, h, w)
+  drawTile(context, 0, 0, texture)
+}
 
-  context.save()
-  context.clearRect(0, 0, canvas.width, canvas.height)
-  context.translate(canvas.width / 2, topBorder)
+const drawCursor = function (context, x0, y0, i, j) {
+  y0 = 130 // correction; magic number; bug patch. :/
+  context.translate(x0, y0)
+
   context.translate((j - i) * TileBase.width / 2, (i + j) * TileBase.height / 2)
   context.beginPath()
   context.moveTo(0, 0)
@@ -109,18 +110,27 @@ const drawCursor = (canvas, i, j) => {
   context.closePath()
   context.fillStyle = 'rgba(0,0,0,0.2)'
   context.fill()
-  context.restore()
 }
 
 const onMove = (cityLayer, interfaceLayer, map, event) => {
-  if (isPlacing) {
-    click(cityLayer, map, event)
+  if (interfaceLayer.isPlacing) {
+    click(interfaceLayer, map, event)
+    drawMap(cityLayer, map)
   }
 
   const { i, j } = getTileIndex(cityLayer, event.offsetX, event.offsetY)
+  if (i >= 0 && i < cityLayer.dimension && j >= 0 && j < cityLayer.dimension) {
+    interfaceLayer.cursorAt = { i, j }
 
-  if ((i >= 0 && i < map.dimension) && (j >= 0 && j < map.dimension)) {
-    drawCursor(interfaceLayer, i, j)
+    const context = interfaceLayer.getContext('2d')
+
+    context.resetTransform()
+    context.clearRect(0, 0, interfaceLayer.width, interfaceLayer.height)
+
+    const texture = map.bank.tiles[interfaceLayer.activeToolId]
+    drawActiveTool(context, texture)
+
+    drawCursor(context, interfaceLayer.width / 2, 0, i, j)
   }
 }
 
@@ -202,8 +212,9 @@ class TileTexture {
     this.id = meta.i
     this.source = { image, u: meta.u, v: meta.v }
     this.alt = meta.d
-    this.l = meta.l
-    this.r = meta.r
+    this.left = meta.l
+    this.right = meta.r
+    this.section = meta.section || 'Misc'
   }
 }
 TileTexture.height = 230
@@ -217,51 +228,60 @@ class TextureBank {
 
   async loadTextures () {
     /* texture from https://opengameart.org/content/isometric-landscape */
-    const files = [{ 
+    const files = [{
       path: 'textures/01_130x66_130x230.png',
-      textures: [
-        // { i: 1, // identifies this tile-texture // note: id numbers are _file-relative_
-        //   d: 'description', // quick description of this tile
-        //   l: 2, // left-rotate-index
-        //   r: 3, // right-rotate-index
-        //   u: 260, // horizontal texture coordinate
-        //   v: 460, // vertical texture coordinate
-        // },
-        {i: 0, d: 'concrete', u: 0, v: 0},
-        {i: 1, d:'Northwest Road', l:2, r:2, u: 260, v: 0},
-        {i: 2, d: "Southwest Road", r:1, l:1, u: 390, v: 0},
-        // { u: 130, v: 0, alt='fountain'}
+      textures: {
+        example: [ // section-name
+          {
+            i: 1, // identifies this tile-texture // note: id numbers are _file-relative_
+            d: 'description', // quick description of this tile
+            l: 2, // left-rotate-index
+            r: 3, // right-rotate-index
+            u: 260, // horizontal texture coordinate
+            v: 460 // vertical texture coordinate
+          }
+        ],
+        Plain: [
+          { i: 0, d: 'concrete', u: 0, v: 0 }
+        ],
+        Road: [
+          { i: 1, d: 'Northwest Road', l: 2, r: 2, u: 260, v: 0 },
+          { i: 2, d: 'Southwest Road', r: 1, l: 1, u: 390, v: 0 },
 
-        // { u: 520, v: 0 },        // { u: 650, v: 0 },
-        // { u: 780, v: 0 },        // { u: 910, v: 0 },
-        // { u: 1040, v: 0 },        // { u: 1170, v: 0 },
-        // { u: 1300, v: 0 },        // { u: 1430, v: 0 },
-        // { u: 0, v: 230 },
-        // { u: 130, v: 230 },        // { u: 260, v: 230 },        // { u: 390, v: 230 },
-        // { u: 520, v: 230 },        // { u: 650, v: 230 },
-        // { u: 780, v: 230 },        // { u: 910, v: 230 },
-        // { u: 1040, v: 230 },        // { u: 1170, v: 230 },        // { u: 1300, v: 230 },
-        // { u: 1430, v: 230 },        // { u: 0, v: 460 },
-        // { u: 130, v: 460 },
-        {i: 26, d: 'Small Road: NE-SE', l: 28, r: 29, u: 260, v: 460},
-        {i: 27, d: 'Small Road: NW-SW', l: 29, r: 28, u: 390, v: 460},
-        {i: 28, d: 'Small Road: SW-SE', l: 27, r: 26, u: 520, v: 460},
-        {i: 29, d: 'Small Road: NW-NE', l: 26, r: 27, u: 650, v: 460},
-        // { u: 780, v: 460 },        // { u: 910, v: 460 },        // { u: 1040, v: 460 },
-        // { u: 1170, v: 460 },        // { u: 1300, v: 460 },        // { u: 1430, v: 460 },
-        // { u: 0, v: 690 },        // { u: 130, v: 690 },        
-        // big road corners
-        // { u: 260, v: 690 },
-        // { u: 390, v: 690 },
-        // { u: 520, v: 690 },
-        // { u: 650, v: 690 },
+          // { u: 520, v: 0 },        // { u: 650, v: 0 },
+          // { u: 780, v: 0 },        // { u: 910, v: 0 },
+          // { u: 1040, v: 0 },        // { u: 1170, v: 0 },
+          // { u: 1300, v: 0 },        // { u: 1430, v: 0 },
+          // { u: 0, v: 230 },
+          // { u: 130, v: 230 },        // { u: 260, v: 230 },        // { u: 390, v: 230 },
+          // { u: 520, v: 230 },        // { u: 650, v: 230 },
+          // { u: 780, v: 230 },        // { u: 910, v: 230 },
+          // { u: 1040, v: 230 },        // { u: 1170, v: 230 },        // { u: 1300, v: 230 },
+          // { u: 1430, v: 230 },        // { u: 0, v: 460 },
+          // { u: 130, v: 460 },
+          { i: 26, d: 'Small Road: NE-SE', l: 28, r: 29, u: 260, v: 460 },
+          { i: 27, d: 'Small Road: NW-SW', l: 29, r: 28, u: 390, v: 460 },
+          { i: 28, d: 'Small Road: SW-SE', l: 27, r: 26, u: 520, v: 460 },
+          { i: 29, d: 'Small Road: NW-NE', l: 26, r: 27, u: 650, v: 460 },
+          // { u: 780, v: 460 },        // { u: 910, v: 460 },        // { u: 1040, v: 460 },
+          // { u: 1170, v: 460 },        // { u: 1300, v: 460 },        // { u: 1430, v: 460 },
+          // { u: 0, v: 690 },        // { u: 130, v: 690 },
 
-        // low walls
-        {i: 42, d: 'Low Wall: NE', l: 45, r: 43, u: 780, v: 690},
-        {i: 43, d: 'Low Wall: NW', l: 42, r: 44, u: 910, v: 690},
-        {i: 44, d: 'Low Wall: SW', l: 43, r: 45, u: 1040, v: 690},
-        {i: 45, d: 'Low Wall: SE', l: 44, r: 42, u: 1170, v: 690},
-
+          // big road corners
+          { i: 30, d: 'Wide Road: XX-XX', l: 30, r: 30, u: 260, v: 690 },
+          { i: 31, d: 'Wide Road: XX-XX', l: 30, r: 30, u: 390, v: 690 },
+          { i: 32, d: 'Wide Road: XX-XX', l: 30, r: 30, u: 520, v: 690 },
+          { i: 33, d: 'Wide Road: XX-XX', l: 30, r: 30, u: 650, v: 690 }
+        ],
+        Water: [
+          // { u: 130, v: 0, alt='fountain'}
+        ],
+        Wall: [
+          { i: 42, d: 'Low Wall: NE', l: 45, r: 43, u: 780, v: 690 },
+          { i: 43, d: 'Low Wall: NW', l: 42, r: 44, u: 910, v: 690 },
+          { i: 44, d: 'Low Wall: SW', l: 43, r: 45, u: 1040, v: 690 },
+          { i: 45, d: 'Low Wall: SE', l: 44, r: 42, u: 1170, v: 690 }
+        ]
         // { u: 1300, v: 690 },        // { u: 1430, v: 690 },        // { u: 0, v: 920 },
         // { u: 130, v: 920 },        // { u: 260, v: 920 },        // { u: 390, v: 920 },
         // { u: 520, v: 920 },        // { u: 650, v: 920 },        // { u: 780, v: 920 },
@@ -272,7 +292,7 @@ class TextureBank {
         // { u: 520, v: 1150 },        // { u: 650, v: 1150 },        // { u: 780, v: 1150 },
         // { u: 910, v: 1150 },        // { u: 1040, v: 1150 },        // { u: 1170, v: 1150 },
         // { u: 1300, v: 1150 },        // { u: 1430, v: 1150}
-      ]
+      }
     }]
 
     const loads = files.map(f => {
@@ -292,75 +312,121 @@ class TextureBank {
       const image = event.target
       const textures = event.target.textures
 
-      const columnCount = image.width / TileTexture.width
-      const rowCount = image.height / TileTexture.height
-
-      console.log("Loading textures...")
+      console.log('Loading textures...')
 
       // load image row-by-row
       let lastTextureId = 0
-      textures.forEach(metadata => {
-        const tex = new TileTexture(image, metadata)
-        this.tiles[tex.id] = tex
+      Object.entries(textures).forEach(sectionEntry => {
+        sectionEntry[1].forEach(metadata => {
+          metadata.section = sectionEntry[0]
+          const tex = new TileTexture(image, metadata)
+          this.tiles[tex.id] = tex
+        })
       })
     })
   }
 }
 
-// ====== ====== ====== ====== Main ====== ====== ====== ======
+// ====== ====== ====== ====== ToolBar ====== ====== ====== ======
 // ==================================================================
-const populateToolbar = (bank) => {
-  const tools = document.querySelector('#tools')
+const getToolContainer = name => {
+  const lowerName = name.toLowerCase()
+  const sectionId = `${lowerName}-section`
+  const contentClass = 'section-content'
+  const toolbar = document.querySelector('#tools')
 
+  let section = toolbar.querySelector('#' + sectionId)
+  if (section) {
+    return section.querySelector('.' + contentClass)
+  } else {
+    // <div id="lorem-section" class="tool-section">
+    section = document.createElement('div')
+    section.id = sectionId
+    section.classList.add('tool-section')
+    toolbar.append(section)
+
+    // <h4> Lorem ipsum dolor sit amet. </h4>
+    const header = document.createElement('h4')
+    header.classList.add('section-header')
+    header.appendChild(document.createTextNode(`${name} Tools`))
+    section.appendChild(header)
+
+    // <div class="section-content">
+    const content = document.createElement('div')
+    content.classList.add(contentClass)
+    section.appendChild(content)
+
+    return content
+  }
+}
+
+const createToolDiv = (tex) => {
+  const div = document.createElement('div')
+  div.classList.add('tile-tool')
+
+  div.id = `tool_${tex.id}`
+  div.style.display = 'block'
+
+  // width of 132 instead of 130  = 130 image + 2 border = 132
+  div.style.backgroundPosition = `-${tex.source.u}px -${tex.source.v}px`
+
+  return div
+}
+
+const populateToolbar = (bank, interfaceLayer) => {
   bank.tiles.forEach(tex => {
-    const div = document.createElement('div')
+    const container = getToolContainer(tex.section)
+    const div = createToolDiv(tex)
 
-    div.id = `tool_${tex.id}`
-    div.style.display = 'block'
-
-    // width of 132 instead of 130  = 130 image + 2 border = 132
-    // div.style.backgroundPosition = `-${j*130+2}px -${i*230}px`
-    div.style.backgroundPosition = `-${tex.source.u}px -${tex.source.v}px`
     div.addEventListener('click', e => {
-      if (activeToolDiv) {
-        document.querySelector(`#${activeToolDiv}`).classList.remove('selected')
-      }
-      activeToolDiv = e.target.id
-      activeTexId = tex.id
-      document.querySelector(`#${activeToolDiv}`).classList.add('selected')
+      interfaceLayer.activeToolId = tex.id
+
+      const context = interfaceLayer.getContext('2d')
+      context.resetTransform()
+      context.clearRect(0, 0, interfaceLayer.width, interfaceLayer.height)
+
+      drawActiveTool(context, tex)
     })
-    tools.appendChild(div)
+
+    container.appendChild(div)
   })
 }
 
-// "main" / entry point:
+// ====== ====== ====== ====== Main ====== ====== ====== ======
+// ============================================================
 (function init () {
   const map = new Map(8)
 
   const cityLayer = document.querySelector('#bg')
-
+  cityLayer.dimension = map.dimension
   // these dimensions are not _exact_ ... but should be a good heuristic :)
-  cityLayer.width = map.dimension * TileBase.width
   cityLayer.height = map.dimension * TileBase.height + TileTexture.height
+  cityLayer.width = map.dimension * TileBase.width
+  cityLayer.isPlacing = false
   console.log(`>> Building canvas: ${cityLayer.width} x ${cityLayer.height}`)
 
   const bank = new TextureBank()
+
+  const interfaceLayer = document.querySelector('#fg')
+  interfaceLayer.dimension = map.dimension
+  interfaceLayer.width = cityLayer.width
+  interfaceLayer.height = cityLayer.height
+  interfaceLayer.activeToolId = 0
+
+  interfaceLayer.addEventListener('contextmenu', e => e.preventDefault())
+  interfaceLayer.addEventListener('mousemove', e => onMove(cityLayer, interfaceLayer, map, e))
+
+  interfaceLayer.addEventListener('mouseup', e => unclick(e, interfaceLayer))
+  interfaceLayer.addEventListener('mousedown', e => { click(interfaceLayer, map, e); drawMap(cityLayer, map) })
 
   bank.loadTextures().then(_ => {
     map.bank = bank
     map.load(document.location.hash.substring(1))
     drawMap(cityLayer, map)
 
-    populateToolbar(bank)
+    populateToolbar(bank, interfaceLayer)
+
+    /* global registerToolHeadings */
+    registerToolHeadings()
   })
-
-  const interfaceLayer = document.querySelector('#fg')
-  interfaceLayer.width = cityLayer.width
-  interfaceLayer.height = cityLayer.height
-
-  interfaceLayer.addEventListener('contextmenu', e => e.preventDefault())
-  interfaceLayer.addEventListener('mousemove', e => onMove(cityLayer, interfaceLayer, map, e))
-
-  interfaceLayer.addEventListener('mouseup', e => unclick(e))
-  interfaceLayer.addEventListener('mousedown', e => click(cityLayer, map, e))
 })()
--- js/sidebar.js
+++ js/sidebar.js
@@ -1,17 +1,23 @@
+/* eslint-disable no-unused-vars */
 
-document.querySelectorAll('.tool-section').forEach(div => {
-  div.addEventListener('click', event => {
-    event.stopPropagation()
-    // console.log("@@ element: ", event.target.closest('.tool-section').id)
+const registerToolHeadings = _ => {
+  document.querySelectorAll('.section-header').forEach(div => {
+    div.addEventListener('click', event => {
+      event.stopPropagation()
 
-    const content = event.target.closest('.tool-section').querySelector('.section-content')
-    // console.log("::content::", content)
-    if (window.getComputedStyle(content).display === 'block') {
-      // hide element
-      content.style.display = 'none'
-    } else {
-      // show element
-      content.style.display = 'block'
-    }
+      // console.log("@@ element: ", event.target.closest('.tool-section').id)
+
+      const content = event.target.closest('.tool-section').querySelector('.section-content')
+
+      // console.log("::content::", content)
+      // console.log(window.getComputedStyle(content).display)
+      if (window.getComputedStyle(content).display === 'grid') {
+        // hide element
+        content.style.display = 'none'
+      } else {
+        // show element
+        content.style.display = 'grid'
+      }
+    })
   })
-})
+}

57a804bf1b57bacbd9030a0dfd1f29866f2d68b9
Daniel_M_Williams <equipoise@gmail.com>
2020-01-19 23:23:09 -0500

    [fix] restores building tiles [feat] which are now SW <-> SE rotatable

--- js/main.js
+++ js/main.js
@@ -322,7 +322,7 @@ const textureFileData = [
         { i: 5, d: 'Basin: NW/SE', h: 115, l: 2, r: 2, u: 650, v: 0 },
         { i: 6, d: 'Basin: NW', h: 115, l: 4, r: 3, u: 780, v: 0 },
 
-        { i: 7, d: 'Solo Fountain', h: 120, l: 7, r: 7, u: 2, v: 114 },
+        { i: 7, d: 'Solo Fountain', h: 120, l: 7, r: 7, u: 0, v: 114 },
 
         { i: 8, d: 'Fountain: NE', h: 120, l: 12, r: 14, u: 130, v: 114 },
         { i: 9, d: 'Fountain: SW/NE', h: 120, l: 13, r: 13, u: 260, v: 114 },
@@ -330,6 +330,54 @@ const textureFileData = [
         { i: 12, d: 'Fountain: SE', h: 120, l: 11, r: 8, u: 520, v: 114 },
         { i: 13, d: 'Fountain: NW/SE', h: 120, l: 9, r: 9, u: 650, v: 114 },
         { i: 14, d: 'Fountain: NW', h: 120, l: 8, r: 11, u: 780, v: 114 }
+      ]
+    }
+  }, {
+    offset: 65,
+    path: 'textures/buildings.png',
+    tools: [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35, 37, 39],
+    textures: {
+      building: [
+        { i: 1, d: '3F Brick Building: SW', h: 241, l: 2, r: 2, u: 0, v: 463 },
+        { i: 2, d: '3F Brick Building: SE', h: 231, l: 1, r: 1, u: 0, v: 705 },
+        { i: 3, d: '4F Brick Building: SE', h: 241, l: 4, r: 4, u: 130, v: 463 },
+        { i: 4, d: '4F Brick Building: SW', h: 231, l: 3, r: 3, u: 130, v: 705 },
+        { i: 5, d: '3F Plain Gray Building: SE', h: 241, l: 6, r: 6, u: 260, v: 463 },
+        { i: 6, d: '3F Plain Gray Building: SW', h: 231, l: 5, r: 5, u: 260, v: 705 },
+        { i: 7, d: '4F Plain Gray Building: SE', h: 241, l: 8, r: 8, u: 390, v: 463 },
+        { i: 8, d: '4F Plain Gray Building: SW', h: 231, l: 7, r: 7, u: 390, v: 705 },
+        { i: 9, d: '3F Peaked Brick Building: SE', h: 241, l: 10, r: 10, u: 520, v: 463 },
+        { i: 10, d: '3F Peaked Brick Building: SW', h: 231, l: 9, r: 9, u: 520, v: 705 },
+        { i: 11, d: '4F Brick: Blue Awning: SE', h: 241, l: 12, r: 12, u: 650, v: 463, w: 128 },
+        { i: 12, d: '4F Brick: Blue Awning: SW', h: 231, l: 11, r: 11, u: 650, v: 705, w: 128 },
+        { i: 13, d: '1F Small Box: Green Awning: SE', h: 241, l: 14, r: 14, u: 780, v: 463 },
+        { i: 14, d: '1F Small Box: Green Awning: SW', h: 231, l: 13, r: 13, u: 780, v: 705 },
+        { i: 15, d: '1F-Brick-Peak-Plain-SE', h: 241, l: 16, r: 16, u: 910, v: 463 },
+        { i: 16, d: '1F-Brick-Peak-Plain-SW', h: 231, l: 15, r: 15, u: 910, v: 705 },
+        { i: 17, d: '2F-Brick-Peaked-BlueAwning-SE', h: 241, l: 18, r: 18, u: 1040, v: 463 },
+        { i: 18, d: '2F-Brick-Peaked-BlueAwning-SW', h: 231, l: 17, r: 17, u: 1040, v: 705 },
+        { i: 19, d: '1F-SmallBox-RedAwning-SE', h: 241, l: 20, r: 20, u: 1170, v: 463, w: 128 },
+        { i: 20, d: '1F-SmallBox-RedAwning-SW', h: 231, l: 19, r: 19, u: 1170, v: 705, w: 128 },
+        { i: 21, d: '3F-Peaked-Brick-OrangeAwning-SE', h: 241, l: 21, r: 21, u: 1300, v: 463 },
+        { i: 22, d: '3F-Peaked-Brick-OrangeAwning-SW', h: 231, l: 22, r: 22, u: 1300, v: 705 },
+        { i: 23, d: '1F-Peaked-Bricked-WhiteRoof-SE', h: 241, l: 24, r: 24, u: 1430, v: 463 },
+        { i: 24, d: '1F-Peaked-Bricked-WhiteRoof-SW', h: 231, l: 23, r: 23, u: 1430, v: 705 },
+        { i: 25, d: '2F-Peaked-Brick-SE', h: 241, l: 26, r: 26, u: 1560, v: 463 },
+        { i: 26, d: '2F-Peaked-Brick-SW', h: 231, l: 25, r: 25, u: 1560, v: 705 },
+        { i: 27, d: '3F-Repeat-Brick-Cement-SE', h: 241, l: 28, r: 28, u: 1690, v: 463 },
+        { i: 28, d: '3F-Repeat-Brick-Cement-SW', h: 231, l: 27, r: 27, u: 1690, v: 705 },
+        { i: 29, d: '2F-Plain-GreenAwning-SE', h: 241, l: 30, r: 30, u: 1820, v: 463 },
+        { i: 30, d: '2F-Plain-GreenAwning-SW', h: 231, l: 29, r: 29, u: 1820, v: 705 },
+        { i: 31, d: '3F-Sleek-Glass-SE', h: 241, l: 32, r: 32, u: 1950, v: 463 },
+        { i: 32, d: '3F-Sleek-Glass-SW', h: 231, l: 31, r: 31, u: 1950, v: 705 },
+        { i: 33, d: '4F-Sleek-Glass-SE', h: 241, l: 34, r: 34, u: 2080, v: 463 },
+        { i: 34, d: '4F-Sleek-Glass-SW', h: 231, l: 33, r: 33, u: 2080, v: 705 },
+        { i: 35, d: '3F-BrownBrick-SE', h: 241, l: 36, r: 36, u: 2210, v: 463 },
+        { i: 36, d: '3F-BrownBrick-SW', h: 231, l: 35, r: 35, u: 2210, v: 705 },
+        { i: 37, d: '3F-Sleek-Glass-SE', h: 241, l: 38, r: 38, u: 2340, v: 463 },
+        { i: 38, d: '3F-Sleek-Glass-SW', h: 231, l: 37, r: 37, u: 2340, v: 705 },
+        { i: 39, d: '1F-Plain-Brick-SE', h: 241, l: 40, r: 40, u: 2470, v: 463 },
+        { i: 40, d: '1F-Plain-Brick-SW', h: 231, l: 39, r: 39, u: 2470, v: 705 }
       ]
     }
   }
@@ -471,7 +519,9 @@ const populateToolbar = (bank, interfaceLayer) => {
     // console.log(`... processing tool # '${toolId}'`)
 
     const tool = bank.tiles[toolId]
-
+    if (!tool) {
+      return
+    }
     // console.log(`Creating Tool #${tool.id} for:'${tool.description}'  in: '${tool.section}'`)
     // const source = tool.source
     // console.log(`    @ [ ${source.u}, ${source.v}]  =>  [ ${source.width}, ${source.height}]`)
